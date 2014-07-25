f5f3935a4514df90d75acab5097ddc61bb3b4262
Gerrard Geldenhuis <gerrard@puppetlabs.com>
2014-07-25 10:35:27 +0100

    Fixing numpty errors

--- manifest/client02.puppetlabs.vm.pp
+++ manifest/client02.puppetlabs.vm.pp
@@ -2,5 +2,5 @@ node 'client02.puppetlabs.vm' {
   include profiles::motd
   notify { 'I am classified!': }
   notify { 'I am doubly classified!': }
-  notify { 'I am doubly classified!': }
+  notify { 'I am triply classified!': }
 }
