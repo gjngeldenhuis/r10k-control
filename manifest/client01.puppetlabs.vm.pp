160d06a14e20a7f6a43bd0fd9fe543847353ed36
Gerrard Geldenhuis <gerrard@puppetlabs.com>
2014-09-01 15:14:02 +0200

    Adding us specific values

--- manifest/client01.puppetlabs.vm.pp
+++ manifest/client01.puppetlabs.vm.pp
@@ -2,5 +2,5 @@ node 'client01.puppetlabs.vm' {
   include profiles::motd
   notify { 'I am classified!': }
   notify { 'I am doubly classified!': }
-  notify { 'This value should only be shown for the bcs branch': }
+  notify { 'This value should only be shown for the bcs US branch': }
 }
